65148f12cecacc2590f1acfe84544ee5e6db5d2a
Jesus Alberto Paz Jimenez <alberto.pazjimenez@gmail.com>
2016-05-23 16:51:41 +0200

    Solved by adding variations

--- src/main/scala/com/test/Solution3.scala
+++ src/main/scala/com/test/Solution3.scala
@@ -8,7 +8,7 @@ object Solution3 {
   def numberOfWays(X:Int,N:Int):Int = {
     // Compute the answer in this function over here
     // It is fine to define new functions as and where required
-    val numbers : Array[Int] = Stream.from(1).map(Math.pow(_,N)).takeWhile(_ < X).map(_.toInt).toArray
+    val numbers : Array[Int] = Stream.from(1).map(Math.pow(_,N)).takeWhile(_ <= X).map(_.toInt).toArray
     val n = numbers.length
 
     val subset = Array.ofDim[Int](X+1, n+1)
@@ -25,13 +25,13 @@ object Solution3 {
       subset(i)(j) = subset(i)(j-1)
       val number : Int = numbers(j-1)
       if (i >= number) {
-        subset(i)(j) = Math.max(subset(i)(j), subset(i-number)(j-1))+1;
+        subset(i)(j) = subset(i)(j) + subset(i-number)(j-1)
       }
     }
 
-    println(subset.map(_.mkString(" ")).mkString("\n"))
+//    println(subset.map(_.mkString(" ")).mkString("\n"))
 
-    subset(X).count(_ == true)
+    subset(X)(n)
   }
 
 
